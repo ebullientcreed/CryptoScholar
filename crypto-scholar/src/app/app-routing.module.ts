ff804730007bc60da2538f0dce1571e53ef479dd
Christopher Barnwell <crist.of@live..com>
2019-11-24 15:14:26 -0600

    Form

--- crypto-scholar/src/app/app-routing.module.ts
+++ crypto-scholar/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BrowseOffersComponent } from './browse-offers/browse-offers.component';
+import { LandingComponent } from './landing/landing.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component:LandingComponent
+  },
+  {
+    redirectTo: '',
+    path: '',
+    pathMatch:'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
